55ead1d855c0bb34cff7d614bd61e346237b2e3a
namori999 <mori091700@gmail.com>
2023-03-01 09:30:12 +0900

    デバッグモードの表示を消す

--- lib/views/privacy_policy_page.dart
+++ lib/views/privacy_policy_page.dart
@@ -39,55 +39,56 @@ class PrivacyPolicyPage extends StatelessWidget {
         elevation: 0,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              '広告について',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
-            const Text(
-                '本アプリでは、広告配信ツールとしてAdMob(Google Inc.)を使用しており、AdMobがご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下の広告配信事業者のアプリケーション・プライバシーポリシーのリンクよりご確認ください。'),
-            const SizedBox(height: 16),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text('Google 広告に関するポリシー', style: TextStyle(fontSize: 12)),
-            ),
-            TextButton(
-              child: const Text(
-                  'https://policies.google.com/technologies/ads?hl=ja'),
-              onPressed: () {
-                launchUrl(
-                  Uri.parse(
-                      'https://policies.google.com/technologies/ads?hl=ja'),
-                );
-              },
-            ),
-            const SizedBox(height: 48),
-            const Text(
-              '利用状況解析ついて',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 24),
-            const Text(
-                '本アプリでは、今後の開発の参考とするため、アプリの利用状況データを収集するツールとしてFirebase(Google Inc.)を使用しており、Firebaseがご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下のGoogleプライバシーポリシーのリンクよりご確認ください。'),
-            const SizedBox(height: 16),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Text('Google プライバシーポリシー', style: TextStyle(fontSize: 12)),
-            ),
-            TextButton(
-              child: const Text('https://policies.google.com/privacy?hl=ja'),
-              onPressed: () {
-                launchUrl(
-                  Uri.parse('https://policies.google.com/privacy?hl=ja'),
-                );
-              },
-            ),
-          ],
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 32), child: SelectionArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                '広告について',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                  '本アプリでは、広告配信ツールとしてAdMob(Google Inc.)を使用しており、AdMobがご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下の広告配信事業者のアプリケーション・プライバシーポリシーのリンクよりご確認ください。'),
+              const SizedBox(height: 16),
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text('Google 広告に関するポリシー', style: TextStyle(fontSize: 12)),
+              ),
+              TextButton(
+                child: const Text(
+                    'https://policies.google.com/technologies/ads?hl=ja'),
+                onPressed: () {
+                  launchUrl(
+                    Uri.parse(
+                        'https://policies.google.com/technologies/ads?hl=ja'),
+                  );
+                },
+              ),
+              const SizedBox(height: 48),
+              const Text(
+                '利用状況解析ついて',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                  '本アプリでは、今後の開発の参考とするため、アプリの利用状況データを収集するツールとしてFirebase(Google Inc.)を使用しており、Firebaseがご利用者の情報を自動取得する場合がございます。取得する情報、利用目的、第三者への提供等につきましては、以下のGoogleプライバシーポリシーのリンクよりご確認ください。'),
+              const SizedBox(height: 16),
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text('Google プライバシーポリシー', style: TextStyle(fontSize: 12)),
+              ),
+              TextButton(
+                child: const Text('https://policies.google.com/privacy?hl=ja'),
+                onPressed: () {
+                  launchUrl(
+                    Uri.parse('https://policies.google.com/privacy?hl=ja'),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
